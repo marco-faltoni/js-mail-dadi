55aea177ab97fd62f97848c9143ea71bbe05c600
Marco <marco.faltoni@outlook.it>
2020-04-21 16:48:05 +0200

    Esercizio 1

--- mail/main.js
+++ mail/main.js
@@ -3,20 +3,28 @@
 
 
 // Creo Array contenente della mail autorizzate facenti parte di una lista
-var email_array = ['oqugedda-4927@yopmail.com', 'asagata@potasf.com', 'ukumuwe-9804@yopmail.com', 'sono invincibile@posta.com', 'sonoinlista@lista.com',];
-console.log(email_array);
+var lista_email = ['oqugedda-4927@yopmail.com','marco@marco.com', 'asagata@potasf.com', 'ukumuwe-9804@yopmail.com', 'sono invincibile@posta.com', 'sonoinlista@lista.com',];
 
 
 // Chiedo all'utente la sua email
 var email_utente = prompt('Digita la tua email per scoprire se sei in lista');
 console.log(email_utente);
 
-// visualizzo/stampo messaggio appropriato (sei in lista/non sei in lista)
-for (var i = 0; i < nomi.length; i++) {
-    console.log('sei in lista');
+// scrivo una variante con il risultato finale da definire
+var risultato;
+
+// imposto con ciclo for se c'è o meno il match con l'email dell'utente
+for (var i = 0; i < lista_email.length; i++) {
+    if (email_utente == lista_email) {
+        risultato = true;
+    } else {
+        risultato = false;
+    }
+    // PS: in teoria il valore else può semplicemente non essere messo, in quando risulterebbe di default false perchè a IF è stato assegnato valore true
 }
 
-for (var i = 0; i < email_utente.length; i++) {
-    email_utente[i]
-    console.log('sei in lista');
+if (risultato == true) {
+    console.log('sei in lista, complimenti');
+} else {
+    console.log('non sei in lista, mi dispiace');
 }
